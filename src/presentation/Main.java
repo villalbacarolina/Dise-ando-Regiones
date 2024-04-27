e5af0ae3cd144d5e5bf3544e3e67b5a5568a8853
Jose Herrera <jooherrera4@gmail.com>
2024-04-27 11:48:37 -0300

    chore: initial commit

---
+++ src/presentation/Main.java
@@ -0,0 +1,49 @@
+package presentation;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Main {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main window = new Main();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Main() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setResizable(false);
+		frame.setBounds(100, 100, 800, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblDiseandoRegiones = new JLabel("Diseñando Regiones");
+		lblDiseandoRegiones.setBounds(332, 53, 136, 17);
+		frame.getContentPane().add(lblDiseandoRegiones);
+	}
+}
